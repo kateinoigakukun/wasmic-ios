6f9cfb777dd829996d2041b13772ea64ffb649ca
Yuta Saito <kateinoigakukun@gmail.com>
2021-04-10 21:13:19 +0900

    Link shared static libraries into one dynamic framework

---
+++ Sources/WasmicKit/WasmicKit.h
@@ -0,0 +1,18 @@
+//
+//  WasmicKit.h
+//  WasmicKit
+//
+//  Created by kateinoigakukun on 2021/04/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WasmicKit.
+FOUNDATION_EXPORT double WasmicKitVersionNumber;
+
+//! Project version string for WasmicKit.
+FOUNDATION_EXPORT const unsigned char WasmicKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WasmicKit/PublicHeader.h>
+
+
---
+++ Sources/WasmicKit/WasmicKit.swift
@@ -0,0 +1,8 @@
+//
+//  WasmicKit.swift
+//  WasmicKit
+//
+//  Created by kateinoigakukun on 2021/04/10.
+//
+
+@_exported import WasmicWasm
