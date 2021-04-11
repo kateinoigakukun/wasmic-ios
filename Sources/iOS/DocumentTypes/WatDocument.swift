90c0993699cb772724f6daca652ca82e760ec302
Yuta Saito <kateinoigakukun@gmail.com>
2021-04-11 18:35:17 +0900

    Add template

--- Sources/iOS/DocumentTypes/WatDocument.swift
+++ Sources/iOS/DocumentTypes/WatDocument.swift
@@ -13,5 +13,13 @@ class WatDocument: TextDocument {
         let url = tempDir.appendingPathComponent("main.wat")
 
         super.init(fileURL: url)
+        text = """
+        (module
+            (export "_start" (func $_start))
+            (func $_start (result i32)
+                (i32.const 0)
+            )
+        )
+        """
     }
 }
